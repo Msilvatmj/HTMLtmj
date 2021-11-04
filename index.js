2fc63d73b77a54753e4614711aea8bd8582a9f13
Msilvatmj <marcossilvabalsasma@gmail.com>
2021-11-04 12:35:15 -0100

    atualização

--- index.js
+++ index.js
@@ -3,6 +3,10 @@
   const app = express()
 
   app.use(express.static('./public'))
+
+  app.get('/',(req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'))
+   })
    
   app.get('/',(req, res) => {
     res.send('Hello World')
